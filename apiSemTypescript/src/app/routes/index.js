fad5d7b09fd0251a02b6e41ecb8b6382eda912d6
fabiocarmovilanova <fabio.carmo@vilanova.com.br>
2023-04-08 16:33:27 -0300

    Insert new route for beerstyle

--- apiSemTypescript/src/app/routes/index.js
+++ apiSemTypescript/src/app/routes/index.js
@@ -1,8 +1,6 @@
 const app = require('../../app');
-const UsuariosRouter = require('../routes/UsuariosRouter');
-const AuthRouter = require('./AuthRoutes');
+const BeerStyleRouter = require('../routes/BeerStyleRouter');
 
 module.exports = (app) => {
-    app.use(UsuariosRouter);
-    app.use(AuthRouter);
+    app.use(BeerStyleRouter);
 }
