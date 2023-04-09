fb87df95b66211426f9c0ec0dfef06e0315a3abd
fabiocarmovilanova <fabio.carmo@vilanova.com.br>
2023-04-09 11:35:16 -0300

    Create a route for find beer using temperature

--- apiSemTypescript/src/app/routes/BeerStyleRouter.js
+++ apiSemTypescript/src/app/routes/BeerStyleRouter.js
@@ -6,6 +6,19 @@ const AuthMiddleware = require('../middlewares/AuthMiddleware');
 BeerStyleRouter.post('/beerstyle', BeerStyleController.create);
 BeerStyleRouter.get('/beerstyle', BeerStyleController.read);
 
+BeerStyleRouter.get('/beerstyle_temperature', async (req, res) => {
+    const { temperature } = req.body;
+    console.log(req.query);
+    if (!temperature) {
+      return res.status(400).send('Temperature is required.');
+    }
+  
+    const beerStyle = await BeerStyleController.findBeerStyleByTemperature(temperature);
+  
+    return res.json(beerStyle);
+  });
+
+
 
 // BeerStyleRouter.patch('/beerstyle/:id', BeerStyleController.update);
 
